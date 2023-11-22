e1ed2c2f76aeb7ebc954b3f12373f9c007fcae99
Xu Che <xuche1998@gmail.com>
2023-11-22 20:28:35 +0800

    implement json encoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -5,8 +5,11 @@ import (
 	"net/http"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+func (app *application) createMovieHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintln(w, "create a new movie")
+	if err != nil {
+		return
+	}
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +17,8 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 	}
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	_, err = fmt.Fprintf(w, "show the details of movie %d\n", id)
+	if err != nil {
+		return
+	}
 }
