3063245673a2f616ed02cc0b6345495ce34194b5
Xu Che <xuche1998@gmail.com>
2023-11-22 20:58:40 +0800

    add json envelope, edit showMovieHandler to use writeJSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/xuche123/greenlight/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, _ *http.Request) {
@@ -16,9 +18,23 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
-	}
-	_, err = fmt.Fprintf(w, "show the details of movie %d\n", id)
-	if err != nil {
 		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request",
+			http.StatusInternalServerError)
 	}
 }
