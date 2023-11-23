bd335391d39425363eebf6a7a80545bca5d80356
Xu Che <xuche1998@gmail.com>
2023-11-23 21:59:14 +0800

    triage errors returned by Decode()

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -7,11 +7,21 @@ import (
 	"time"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, _ *http.Request) {
-	_, err := fmt.Fprintln(w, "create a new movie")
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
 	if err != nil {
+		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
