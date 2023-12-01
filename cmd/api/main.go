571bbcccc575abc910f1562441f2ec3ab0fe585e
xuche123 <xuche1998@gmail.com>
2023-12-01 21:49:13 +0800

    implement sending emails

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,7 +6,9 @@ import (
 	"flag"
 	"github.com/xuche123/greenlight/internal/data"
 	"github.com/xuche123/greenlight/internal/jsonlog"
+	"github.com/xuche123/greenlight/internal/mailer"
 	"os"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -28,12 +30,21 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -50,6 +61,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "8611f64c8cff67", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "cc7f736f65f716", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -83,6 +100,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
--- go.mod
+++ go.mod
@@ -8,4 +8,8 @@ require (
 	golang.org/x/time v0.5.0
 )
 
-require golang.org/x/crypto v0.16.0 // indirect
+require (
+	github.com/go-mail/mail/v2 v2.3.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
